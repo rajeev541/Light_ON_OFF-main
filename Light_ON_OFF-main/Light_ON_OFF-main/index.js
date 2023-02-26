94fb8a39ace1a6675b2fd1fcebc152be8e4e06e8
Rajeev Singh Rawat <71057442+rajeev541@users.noreply.github.com>
2023-02-26 17:47:38 +0530

    Add files via upload

---
+++ Light_ON_OFF-main/Light_ON_OFF-main/index.js
@@ -0,0 +1,7 @@
+function turnOn() {
+  document.getElementById("myImage").src = "./images/pic_bulbon.gif";
+}
+
+function turnOff() {
+  document.getElementById("myImage").src = "./images/pic_bulboff.gif";
+}
